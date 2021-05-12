9445efe20be1036cd33db0989d954bedaabe7fd0
Viniciu Cristian <git.viniciu@gmail.com>
2021-05-12 20:55:48 -0300

    :tada: update

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -21,32 +21,31 @@ export class DashboardPage{
   ngOnInit() {
     this.users = new User();
     this.afa.authState.subscribe(user =>{
-        this.userid = user.uid;
-         this.username = user.displayName;
-        // console.log(this.userid);
-        // console.log(this.username)
-
-        if(this.userid){
-          const subscribe = this.dashboardservice.getById(this.userid).subscribe( (data: any) =>{
-        subscribe.unsubscribe();
-        console.log(data);
-        //const { name, datanascimento, cartaosus, tiposanguineo, contato, email, comorbidades, filePath, imgUrl } = data;
-        this.users.name = data.name == null ? "" : data.name;
-        this.users.address= data.address == null ? "" : data.address;
-        this.users.address_number = data.address_number == null ? "" : data.address_number;
-        this.users.address_district = data.address_district == null ? "" : data.address_district;
-        this.users.address_city = data.address_city == null ? "" : data.address_city;
-        this.users.address_state = data.address_state == null ? "" : data.address_state;
-        this.users.cartaosus = data.cartaosus == null ? "" : data.cartaosus;
-        this.users.tiposanguineo = data.tiposanguineo == null ? "" : data.tiposanguineo;
-        this.users.contato = data.contato == null ? "" : data.contato;
-        this.users.email = data.email == null ? "" : data.email;
-        this.users.comorbidades = data.comorbidades == null ? "" : data.comorbidades;
-        this.users.filePath = data.filePath == null ? "" : data.filePath;
-        this.users.imgUrl = data.imgUrl == null ? "" : data.imgUrl;
-        this.hasImg = this.users.imgUrl == '' ? true : false;
-      });
-      }
+      this.userid = user.uid;
+       this.username = user.displayName;
+       this.viewUser();
     })
  }
+  viewUser(){
+    if(this.userid){
+      const subscribe = this.dashboardservice.getById(this.userid).subscribe( (data: any) =>{
+    subscribe.unsubscribe();
+    console.log(data);
+    this.users.name = data.name == null ? "" : data.name;
+    this.users.address= data.address == null ? "" : data.address;
+    this.users.address_number = data.address_number == null ? "" : data.address_number;
+    this.users.address_district = data.address_district == null ? "" : data.address_district;
+    this.users.address_city = data.address_city == null ? "" : data.address_city;
+    this.users.address_state = data.address_state == null ? "" : data.address_state;
+    this.users.cartaosus = data.cartaosus == null ? "" : data.cartaosus;
+    this.users.tiposanguineo = data.tiposanguineo == null ? "" : data.tiposanguineo;
+    this.users.contato = data.contato == null ? "" : data.contato;
+    this.users.email = data.email == null ? "" : data.email;
+    this.users.comorbidades = data.comorbidades == null ? "" : data.comorbidades;
+    this.users.filePath = data.filePath == null ? "" : data.filePath;
+    this.users.imgUrl = data.imgUrl == null ? "" : data.imgUrl;
+    this.hasImg = this.users.imgUrl == '' ? true : false;
+  });
+  }
+  }
 }
